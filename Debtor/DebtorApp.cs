c95652682de3f9ca8c91812a9694190c8020ac22
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-08-03 19:34:53 +0200

    Add files via upload

---
+++ Debtor/DebtorApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debtor
+{
+    public class DebtorApp
+    {
+        public void IntroduceDebtorApp()
+        {
+
+        }
+        public void AddBorrower()
+        {
+
+        }
+        public void DeleteBorrower()
+        {
+
+        }
+        public void ListAllBorrowers()
+        {
+
+        }
+        public void AskForAction()
+        {
+
+        }
+    }
+}
